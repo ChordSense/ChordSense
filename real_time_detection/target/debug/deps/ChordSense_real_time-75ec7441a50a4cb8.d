a266c21d05565f5edb52a6b6df62cac960ff42e7
tjcanro <79056896+tjcanro@users.noreply.github.com>
2026-04-09 01:36:02 -0400

    Completed smoothing and prepared skeleton for project structure

---
+++ real_time_detection/target/debug/deps/ChordSense_real_time-75ec7441a50a4cb8.d
@@ -0,0 +1,5 @@
+/Users/thomascanro/Desktop/CLASSES/CPEDesign/ChordSenseDisplay/real_time_detection/target/debug/deps/ChordSense_real_time-75ec7441a50a4cb8.d: src/main.rs
+
+/Users/thomascanro/Desktop/CLASSES/CPEDesign/ChordSenseDisplay/real_time_detection/target/debug/deps/libChordSense_real_time-75ec7441a50a4cb8.rmeta: src/main.rs
+
+src/main.rs:
