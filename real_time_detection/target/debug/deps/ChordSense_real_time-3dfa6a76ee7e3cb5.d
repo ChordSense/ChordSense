674c8942d1c316a16efc503173b6250c9e7f6a7c
tjcanro <79056896+tjcanro@users.noreply.github.com>
2026-04-09 10:51:34 -0400

    misc

--- real_time_detection/target/debug/deps/ChordSense_real_time-3dfa6a76ee7e3cb5.d
+++
@@ -1,5 +0,0 @@
-/Users/thomascanro/Desktop/CLASSES/CPEDesign/ChordSenseDisplay/real_time_detection/target/debug/deps/ChordSense_real_time-3dfa6a76ee7e3cb5.d: src/main.rs
-
-/Users/thomascanro/Desktop/CLASSES/CPEDesign/ChordSenseDisplay/real_time_detection/target/debug/deps/libChordSense_real_time-3dfa6a76ee7e3cb5.rmeta: src/main.rs
-
-src/main.rs:
